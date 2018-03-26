0a48dee7d9e493313826f44955d35a7e91790014
Bago <github@bago.org>
2018-03-26 16:11:50 +0200

    Rename mml to htmml

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 "use strict";
 
-var mml2html = require('htmml');
+var htmml2html = require('htmml');
 var makeThumbs = require('./makeThumbs');
 var checkTemplateDefs = require('./checkTemplateDefs');
 var fse = require('fs-extra');
 
 var templates = [ { 
-	'mml': './template-def/template-versafix-1.mml', 
+	'htmml': './template-def/template-versafix-1.htmml', 
 	'html': './dist/template/template-versafix-1.html',
 	'imgDir': './template-def/img/', 
 	'destImgDir': './dist/template/img/', 
 	'modelPrefix': './model/template-versafix-1'
 }, {
-	'mml': './template-def/template-versafix-1.it.mml',
+	'htmml': './template-def/template-versafix-1.it.htmml',
 	'html': './dist/template.it/template-versafix-1.it.html',
 	'imgDir': './template-def/img/', 
 	'destImgDir': './dist/template.it/img/', 
@@ -21,7 +21,7 @@ var templates = [ {
 ];
 
 for (var i = 0; i < templates.length; i++) {
-	mml2html(templates[i].mml, templates[i].html);
+	htmml2html(templates[i].htmml, templates[i].html);
 	fse.copySync(templates[i].imgDir, templates[i].destImgDir);
 	checkTemplateDefs(templates[i].html, templates[i].modelPrefix);
 	makeThumbs(templates[i].html, './edres/', 680, 340);
